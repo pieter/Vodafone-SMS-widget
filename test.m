206928ed2e7df6e23c116ac88fb2586f0956b2d6
Pieter de Bie <pdebie@ai.rug.nl>
2009-11-27 00:04:50 +0100

    Remove dead code

--- test.m
+++ test.m
@@ -3,16 +3,9 @@
 
 int main (int argc, char const *argv[])
 {
-	if (argc < 2) {
-		printf("Usage: %s <name>\n", argv[0]);
-		exit(1);
-	}
-
 	NSArray *peopleFound = [[[PBAddressBook alloc] init] numbers];
 	for (NSArray *n in peopleFound)
 		printf("%s -- %s (%s)\n", [[n objectAtIndex:0] UTF8String], [[n objectAtIndex:2] UTF8String], [[n objectAtIndex:1] UTF8String]);
-	
-	// NSLog(@"Persons: %@", peopleFound);
 
 	return 0;
 }
